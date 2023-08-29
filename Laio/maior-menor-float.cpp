2c951db8cb0f06ebdc1186fb9c2a26aaf37ce341
OutLaio <rlaio.ege@gmail.com>
2023-08-29 17:37:57 -0300

    Questão 2, Letra c

--- Laio/maior-menor-float.cpp
+++ Laio/maior-menor-float.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<limits>
+#include<iomanip>
 
 using namespace std;
 
@@ -7,6 +8,16 @@ int main(){
     
     float pif = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
 
+    cout << fixed << setprecision(2);
+    cout << "Valor de pi: " << pif << endl;
+
+    cout << fixed << setprecision(4);
+    cout << "Valor de pi: " << pif << endl;
+
+    cout << fixed << setprecision(8);
+    cout << "Valor de pi: " << pif << endl;
+
+    cout << fixed << setprecision(16);
     cout << "Valor de pi: " << pif << endl;
 
     return 0;
