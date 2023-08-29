9fd607240600a4f4fa8dd092b7efd1aa6b0f8a8b
OutLaio <rlaio.ege@gmail.com>
2023-08-29 17:31:04 -0300

    Questão 2, Letra a

---
+++ Laio/maior-menor-float.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<limits>
+
+using namespace std;
+
+int main(){
+    
+    cout << "Menor valor de um float: " << numeric_limits<float>::min() << endl;
+    cout << "Maior valor de um float: " << numeric_limits<float>::max() << endl;
+
+    return 0;
+}
