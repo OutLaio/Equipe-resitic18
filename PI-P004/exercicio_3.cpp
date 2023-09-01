02fa5d37e964e2c5de5e50e9b3ea9e0b1aa3ba41
OutLaio <rlaio.ege@gmail.com>
2023-09-01 18:14:22 -0300

    Exercício 3 - Letra c)

--- PI-P004/exercicio_3.cpp
+++ PI-P004/exercicio_3.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 int main(){
+    long int li = 0xFFFFFFFFFFFFFFFF; 
+    // Devido a li ser do tipo signed, este número corresponde a o maior número negativo que no caso é -1.
 
-    unsigned long int uli = 0xFFFFFFFFFFFFFFFF;
+    unsigned long int uli = li;
+    // Para o tipo unsigned, corresponde ao maior número que pode ser escrito.
 
-    cout << "O maior valor de um unsigned long int eh: " << uli << endl;
+    cout << "O valor de li eh: " << li << endl;
 
-    uli++;
-
-    cout << "O menor valor de um unsigned long int eh: " << uli << endl;
+    cout << "O valor de uli eh: " << uli << endl;
 
     return 0;
 }
