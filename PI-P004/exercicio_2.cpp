c432ab920ca5143132a982d3d10f546a6b301d17
OutLaio <rlaio.ege@gmail.com>
2023-09-01 18:05:29 -0300

    Exercício 2 - Letra c)

--- PI-P004/exercicio_2.cpp
+++ PI-P004/exercicio_2.cpp
@@ -4,29 +4,12 @@
 using namespace std;
 
 int main(){
-    char a, b, c, d, e, f, g, h, i, j;
+    char a;
 
-    a = '0';
-    b = '1';
-    c = '2';
-    d = '3';
-    e = '4';
-    f = '5';
-    g = '6';
-    h = '7';
-    i = '8';
-    j = '9';
-
+    cout << "Digite um caractere: ";
+    cin >> a;
+    
     cout << "'" << a << "' - " << dec << (int)a << " (dec) - " << oct << (int)a << " (oct) - 0x" << hex << (int)a << " (hex) " << endl;
-    cout << "'" << b << "' - " << dec << (int)b << " (dec) - " << oct << (int)b << " (oct) - 0x" << hex << (int)b << " (hex) " << endl;
-    cout << "'" << c << "' - " << dec << (int)c << " (dec) - " << oct << (int)c << " (oct) - 0x" << hex << (int)c << " (hex) " << endl;
-    cout << "'" << d << "' - " << dec << (int)d << " (dec) - " << oct << (int)d << " (oct) - 0x" << hex << (int)d << " (hex) " << endl;
-    cout << "'" << e << "' - " << dec << (int)e << " (dec) - " << oct << (int)e << " (oct) - 0x" << hex << (int)e << " (hex) " << endl;
-    cout << "'" << f << "' - " << dec << (int)f << " (dec) - " << oct << (int)f << " (oct) - 0x" << hex << (int)f << " (hex) " << endl;
-    cout << "'" << g << "' - " << dec << (int)g << " (dec) - " << oct << (int)g << " (oct) - 0x" << hex << (int)g << " (hex) " << endl;
-    cout << "'" << h << "' - " << dec << (int)h << " (dec) - " << oct << (int)h << " (oct) - 0x" << hex << (int)h << " (hex) " << endl;
-    cout << "'" << i << "' - " << dec << (int)i << " (dec) - " << oct << (int)i << " (oct) - 0x" << hex << (int)i << " (hex) " << endl;
-    cout << "'" << j << "' - " << dec << (int)j << " (dec) - " << oct << (int)j << " (oct) - 0x" << hex << (int)j << " (hex) " << endl;
 
     return 0;
 }
