cd966ea93bfce9fc483c4599c5e6273de0690e3f
OutLaio <rlaio.ege@gmail.com>
2023-09-01 17:08:58 -0300

    Exercício 2 - Letra a)

---
+++ PI-P004/exercicio_2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    char a, b, c, d, e, f, g, h, i, j;
+
+    a = '0';
+    b = '1';
+    c = '2';
+    d = '3';
+    e = '4';
+    f = '5';
+    g = '6';
+    h = '7';
+    i = '8';
+    j = '9';
+
+    cout << "'" << a << "' - " << (int)a << endl;
+    cout << "'" << b << "' - " << (int)b << endl;
+    cout << "'" << c << "' - " << (int)c << endl;
+    cout << "'" << d << "' - " << (int)d << endl;
+    cout << "'" << e << "' - " << (int)e << endl;
+    cout << "'" << f << "' - " << (int)f << endl;
+    cout << "'" << g << "' - " << (int)g << endl;
+    cout << "'" << h << "' - " << (int)h << endl;
+    cout << "'" << i << "' - " << (int)i << endl;
+    cout << "'" << j << "' - " << (int)j << endl;
+
+    return 0;
+}
