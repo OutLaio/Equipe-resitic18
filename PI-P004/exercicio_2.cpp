30c2a1d3e8511e5f0fd9f8f773889e819fd03b6a
OutLaio <rlaio.ege@gmail.com>
2023-09-01 17:19:17 -0300

    Exercício 2 - Letra b)

--- PI-P004/exercicio_2.cpp
+++ PI-P004/exercicio_2.cpp
@@ -17,16 +17,16 @@ int main(){
     i = '8';
     j = '9';
 
-    cout << "'" << a << "' - " << (int)a << endl;
-    cout << "'" << b << "' - " << (int)b << endl;
-    cout << "'" << c << "' - " << (int)c << endl;
-    cout << "'" << d << "' - " << (int)d << endl;
-    cout << "'" << e << "' - " << (int)e << endl;
-    cout << "'" << f << "' - " << (int)f << endl;
-    cout << "'" << g << "' - " << (int)g << endl;
-    cout << "'" << h << "' - " << (int)h << endl;
-    cout << "'" << i << "' - " << (int)i << endl;
-    cout << "'" << j << "' - " << (int)j << endl;
+    cout << "'" << a << "' - " << dec << (int)a << " (dec) - " << oct << (int)a << " (oct) - 0x" << hex << (int)a << " (hex) " << endl;
+    cout << "'" << b << "' - " << dec << (int)b << " (dec) - " << oct << (int)b << " (oct) - 0x" << hex << (int)b << " (hex) " << endl;
+    cout << "'" << c << "' - " << dec << (int)c << " (dec) - " << oct << (int)c << " (oct) - 0x" << hex << (int)c << " (hex) " << endl;
+    cout << "'" << d << "' - " << dec << (int)d << " (dec) - " << oct << (int)d << " (oct) - 0x" << hex << (int)d << " (hex) " << endl;
+    cout << "'" << e << "' - " << dec << (int)e << " (dec) - " << oct << (int)e << " (oct) - 0x" << hex << (int)e << " (hex) " << endl;
+    cout << "'" << f << "' - " << dec << (int)f << " (dec) - " << oct << (int)f << " (oct) - 0x" << hex << (int)f << " (hex) " << endl;
+    cout << "'" << g << "' - " << dec << (int)g << " (dec) - " << oct << (int)g << " (oct) - 0x" << hex << (int)g << " (hex) " << endl;
+    cout << "'" << h << "' - " << dec << (int)h << " (dec) - " << oct << (int)h << " (oct) - 0x" << hex << (int)h << " (hex) " << endl;
+    cout << "'" << i << "' - " << dec << (int)i << " (dec) - " << oct << (int)i << " (oct) - 0x" << hex << (int)i << " (hex) " << endl;
+    cout << "'" << j << "' - " << dec << (int)j << " (dec) - " << oct << (int)j << " (oct) - 0x" << hex << (int)j << " (hex) " << endl;
 
     return 0;
 }
