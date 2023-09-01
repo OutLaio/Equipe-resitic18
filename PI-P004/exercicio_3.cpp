e435f8edc99774c9a2b2a7f6e0610c628172ff84
OutLaio <rlaio.ege@gmail.com>
2023-09-01 18:10:59 -0300

    Exercício 3 - Letra a)

---
+++ PI-P004/exercicio_3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int maior = 0x7FFFFFFF;
+    int menor = 0x80000000;
+
+    cout << "O maior valor de inteiro: " << maior << endl;
+    cout << "O menor valor de inteiro: " << menor << endl;
+
+    return 0;
+}
