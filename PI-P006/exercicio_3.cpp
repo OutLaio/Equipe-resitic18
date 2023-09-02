94ef102680f429b334a86f703e1bd122c459fdcb
OutLaio <rlaio.ege@gmail.com>
2023-09-02 17:31:07 -0300

    Exercício 3 - Letra d)

--- PI-P006/exercicio_3.cpp
+++ PI-P006/exercicio_3.cpp
@@ -16,5 +16,10 @@ int main(){
     cout << endl << "O caractere antecessor ao primeiro corresponde a: " << dec << (int)ch3 << " (dec) - "
         << oct << (int)ch3 << " (oct) - 0x" << hex << (int)ch3 << " (hex) - '" << ch3 << "'" << endl << endl;
 
+    ch3 = (ch2 >= 126) ? '_' : ch2+1;
+
+    cout << endl << "O caractere precessor ao segundo corresponde a: " << dec << (int)ch3 << " (dec) - "
+        << oct << (int)ch3 << " (oct) - 0x" << hex << (int)ch3 << " (hex) - '" << ch3 << "'" << endl << endl;
+
     return 0;
 }
