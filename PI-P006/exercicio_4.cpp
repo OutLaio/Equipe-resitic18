53fac4e69e2e86a88f38f2af54eeac4e224885fe
OutLaio <rlaio.ege@gmail.com>
2023-09-02 20:15:43 -0300

    Exercício 4 - Letra d)

--- PI-P006/exercicio_4.cpp
+++ PI-P006/exercicio_4.cpp
@@ -11,9 +11,9 @@ int main(){
     cout << "Digite outro numero: ";
     cin >> y;
 
-    z = x+y;
+    z = (x+y)/2;
 
-    cout << endl << "A soma de x e y eh: " << z << endl;
+    cout << endl << "A media de x e y eh: " << z << endl;
 
     return 0;
 }
