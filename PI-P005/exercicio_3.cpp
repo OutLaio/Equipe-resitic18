dea2ffd9ad8044ce09d69c26566ffa8e9409af14
OutLaio <rlaio.ege@gmail.com>
2023-09-02 12:47:09 -0300

    Exercício 3 - Letra f)

--- PI-P005/exercicio_3.cpp
+++ PI-P005/exercicio_3.cpp
@@ -10,7 +10,8 @@ int main(){
     cout << "Digite outro numero: ";
     cin >> b;
 
-    c = (a>b) ? a-b : b-a;
+    c = abs(a-b);
+    // A função abs retorna o valor absoluto, em outras palavras o módulo de um valor
 
     cout << "A diferenca absoluta entre os dois numeros eh : " << c <<endl;
 
