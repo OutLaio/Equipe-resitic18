738e7bb2227eab93dd6368c32f41c1cf39731870
OutLaio <rlaio.ege@gmail.com>
2023-09-02 12:26:49 -0300

    Exercício 2 - Letra d)

--- PI-P005/exercicio_2.cpp
+++ PI-P005/exercicio_2.cpp
@@ -11,7 +11,16 @@ int main(){
     cout << "Digite um caractere: ";
     cin >> ch1;
 
-    i = (ch1 >= 48 && ch1 <= 57) ? 3 : (ch1 >= 97 && ch1 <= 122) ? 2 : (ch1 >= 65 && ch1 <= 90) ? 1 : 4;
+    // i = isdigit(ch1) ? 3 : islower(ch1) ? 2 : isupper(ch1) ? 1 : 4;
+    /*  as funções isdigit, islower e isupper verificam se a variável possui caractere do tipo:
+        dígito, minusculo e maiusculo, respectivamente, e retornam true ou false */
+
+    i = isalnum(ch1);
+    /*  a função is alnum retorna:
+        1, se a variável possuir caractere maiúsculo;
+        2, se a variável possuir caractere minúsculo;
+        4, se a variável possuir caractere numérico;
+        0, se a variável possuir qualquer outro caractere;*/
 
     cout << "O caractere digitado eh ";
     switch (i){
@@ -21,10 +30,10 @@ int main(){
         case 2:
             cout << "uma letra minuscula;" << endl;
             break;
-        case 3:
+        case 4:
             cout << "um digito;" << endl;
             break;
-        case 4:
+        case 0:
             cout << "outro tipo de caractere;" << endl;
             break;
         default:
