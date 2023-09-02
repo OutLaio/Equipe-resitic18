476454075c3d4b58e66ed873c0026cfc1e1a862a
OutLaio <rlaio.ege@gmail.com>
2023-09-02 12:44:35 -0300

    Exercício 3 - Letra e)

--- PI-P005/exercicio_3.cpp
+++ PI-P005/exercicio_3.cpp
@@ -10,9 +10,9 @@ int main(){
     cout << "Digite outro numero: ";
     cin >> b;
 
-    c = a*b;
+    c = (a>b) ? a-b : b-a;
 
-    cout << "O produto dos dois numeros em octal eh : " << oct << c <<endl;
+    cout << "A diferenca absoluta entre os dois numeros eh : " << c <<endl;
 
     return 0;
 }
