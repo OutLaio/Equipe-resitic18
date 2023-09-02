dd23e1b9f2562dfcd113142d0216a64390a981a2
OutLaio <rlaio.ege@gmail.com>
2023-09-02 20:27:31 -0300

    Exercício 4.1 - Letra a)

---
+++ PI-P006/exercicio_4.1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    double raio;
+
+    cout << endl << "Digite o valor do raio: ";
+    cin >> raio;
+
+    cout << "O diametro eh : " << 2*raio << endl;
+
+    return 0;
+}
