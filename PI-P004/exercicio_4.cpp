ceccd50a185a89437bc75a3cf639eda3b486c19a
OutLaio <rlaio.ege@gmail.com>
2023-09-02 11:24:02 -0300

    Exercício 4 - Letra e)

--- PI-P004/exercicio_4.cpp
+++ PI-P004/exercicio_4.cpp
@@ -6,9 +6,24 @@ using namespace std;
 
 int main(){
 
+    float pif = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
     double pid = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
-;
-    cout << "Valor de pi: " << pid << endl;
+
+    cout << fixed << setprecision(2);
+    cout << "Valor de pi (float): " << pif << endl;
+    cout << "Valor de pi (double): " << pid << endl;
+
+    cout << fixed << setprecision(4);
+    cout << "Valor de pi (float): " << pif << endl;
+    cout << "Valor de pi (double): " << pid << endl;
+
+    cout << fixed << setprecision(8);
+    cout << "Valor de pi (float): " << pif << endl;
+    cout << "Valor de pi (double): " << pid << endl;
+
+    cout << fixed << setprecision(16);
+    cout << "Valor de pi (float): " << pif << endl;
+    cout << "Valor de pi (double): " << pid << endl;
 
     return 0;
 }
