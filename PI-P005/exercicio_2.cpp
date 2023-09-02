aed877db8123b9403bd6a0ed34bf426f099cb4f3
OutLaio <rlaio.ege@gmail.com>
2023-09-02 12:10:46 -0300

    Exercício 2 - Letra c)

--- PI-P005/exercicio_2.cpp
+++ PI-P005/exercicio_2.cpp
@@ -6,9 +6,30 @@ using namespace std;
 int main(){
 
     char ch1, ch2, ch3;
+    int i;
 
     cout << "Digite um caractere: ";
     cin >> ch1;
+
+    i = (ch1 >= 48 && ch1 <= 57) ? 3 : (ch1 >= 97 && ch1 <= 122) ? 2 : (ch1 >= 65 && ch1 <= 90) ? 1 : 4;
+
+    cout << "O caractere digitado eh ";
+    switch (i){
+        case 1:
+            cout << "uma letra maiuscula;" << endl;
+            break;
+        case 2:
+            cout << "uma letra minuscula;" << endl;
+            break;
+        case 3:
+            cout << "um digito;" << endl;
+            break;
+        case 4:
+            cout << "outro tipo de caractere;" << endl;
+            break;
+        default:
+            break;
+    }
 
     return 0;
 }
