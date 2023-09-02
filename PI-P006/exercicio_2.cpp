8ad33a409f6fd8a8646bac7d338715485b2bac15
OutLaio <rlaio.ege@gmail.com>
2023-09-02 17:11:32 -0300

    Exercício 2 - Letra f)

--- PI-P006/exercicio_2.cpp
+++ PI-P006/exercicio_2.cpp
@@ -11,7 +11,16 @@ int main(){
     cout << "Digite outro valor inteiro: ";
     cin >> b;
 
-    c = 4*(a + b)/(3 - 5);
+    //c = 4*(a + b)/(3 - 5);
+    /*  Nesta expressão, primeiro é calculado o que está entre parênteses, 
+        logo em seguida, calcula-se '*' e '/', na ordem em que aparecem primeiro,
+        por fim, calcula-se '+' e '-'. */
+
+    /*  Na expressão anterior, 4*a + b/3 - 5, não há parênteses, logo os valores da multiplicação
+        e divisão serão diferentes. */
+
+    c = 4*(a + b/3) - 5;
+    /*  Nesta equação, a ordem dos calculos são diferentes das anteriores, gerando um novo resultado. */
 
     cout << endl << "O resultado da expressao eh: " << c << endl << endl;
 
