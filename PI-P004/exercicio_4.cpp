8eae5f2733b17b6449042034671f9dc5d90f390a
OutLaio <rlaio.ege@gmail.com>
2023-09-02 11:24:46 -0300

    Exercício 4 - Letra f)

--- PI-P004/exercicio_4.cpp
+++ PI-P004/exercicio_4.cpp
@@ -6,24 +6,14 @@ using namespace std;
 
 int main(){
 
-    float pif = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
+    long double pild = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
     double pid = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
 
-    cout << fixed << setprecision(2);
-    cout << "Valor de pi (float): " << pif << endl;
+    cout << fixed << setprecision(50);
+    cout << "Valor de pi (long double): " << pild << endl;
     cout << "Valor de pi (double): " << pid << endl;
 
-    cout << fixed << setprecision(4);
-    cout << "Valor de pi (float): " << pif << endl;
-    cout << "Valor de pi (double): " << pid << endl;
-
-    cout << fixed << setprecision(8);
-    cout << "Valor de pi (float): " << pif << endl;
-    cout << "Valor de pi (double): " << pid << endl;
-
-    cout << fixed << setprecision(16);
-    cout << "Valor de pi (float): " << pif << endl;
-    cout << "Valor de pi (double): " << pid << endl;
+    // Aparentemente NÃO há diferença entre Double e Long Double
 
     return 0;
 }
