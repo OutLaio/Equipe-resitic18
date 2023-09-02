bbe536ac68cc9871b9c4dbe394bc0c7d95dc44e3
OutLaio <rlaio.ege@gmail.com>
2023-09-02 17:27:39 -0300

    Exercício 3 - Letra c)

--- PI-P006/exercicio_3.cpp
+++ PI-P006/exercicio_3.cpp
@@ -11,5 +11,10 @@ int main(){
     cout << "Digite outro caractere : ";
     cin >> ch2;
 
+    ch3 = (ch1 <= 32) ? '_' : ch1-1;
+
+    cout << endl << "O caractere antecessor ao primeiro corresponde a: " << dec << (int)ch3 << " (dec) - "
+        << oct << (int)ch3 << " (oct) - 0x" << hex << (int)ch3 << " (hex) - '" << ch3 << "'" << endl << endl;
+
     return 0;
 }
