52d785d8f7f72a399bab152a1d558ad7b1babe4c
OutLaio <rlaio.ege@gmail.com>
2023-09-02 16:31:41 -0300

    Exercício 5 - Letra c)

--- PI-P005/exercicio_5.cpp
+++ PI-P005/exercicio_5.cpp
@@ -6,16 +6,26 @@ using namespace std;
 
 int main(){
     unsigned char gene;
-    int qtd = 0, i;
+    int qtd = 0, aux = 0, i, geneEspecifico;
     
     cout << endl << "Digite o caractere correspondente ao gene: ";
     cin >> gene;
+    cout << "Informe o gene especifico que deseja procurar (1 a 8): ";
+    cin >> geneEspecifico;
 
     bitset<8> x(gene);
-    cout << "Os genes possuem a seguinte composicao: " << x << endl;
+    cout << endl << "Os genes possuem a seguinte composicao: " << x << endl;
 
     for (i = 0; i < 8; i++){
         qtd += ((gene & 1<<i) == (1<<i)) ? 1 : 0;
+        if(geneEspecifico == 8-i){
+            if (aux == qtd){
+                cout << "O gene especifico nao esta presente!" << endl;
+            }else{
+                cout << "O gene especifico esta presente!" << endl;
+            }
+        }
+        aux = qtd;
     }
 
     cout << "Estao presentes " << qtd << " genes estudados!" << endl << endl;
