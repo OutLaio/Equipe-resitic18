d2396dfeb7b04a079b2be2e364afbc5cc40040a9
OutLaio <rlaio.ege@gmail.com>
2023-09-02 17:34:59 -0300

    Exercício 3 - Letra e)

--- PI-P006/exercicio_3.cpp
+++ PI-P006/exercicio_3.cpp
@@ -11,14 +11,9 @@ int main(){
     cout << "Digite outro caractere : ";
     cin >> ch2;
 
-    ch3 = (ch1 <= 32) ? '_' : ch1-1;
+    ch3 = (ch1 >= 65 && ch1 <= 90) ? 'A' : ' ';
 
-    cout << endl << "O caractere antecessor ao primeiro corresponde a: " << dec << (int)ch3 << " (dec) - "
-        << oct << (int)ch3 << " (oct) - 0x" << hex << (int)ch3 << " (hex) - '" << ch3 << "'" << endl << endl;
-
-    ch3 = (ch2 >= 126) ? '_' : ch2+1;
-
-    cout << endl << "O caractere precessor ao segundo corresponde a: " << dec << (int)ch3 << " (dec) - "
+    cout << endl << "O caractere ch3 corresponde a: " << dec << (int)ch3 << " (dec) - "
         << oct << (int)ch3 << " (oct) - 0x" << hex << (int)ch3 << " (hex) - '" << ch3 << "'" << endl << endl;
 
     return 0;
