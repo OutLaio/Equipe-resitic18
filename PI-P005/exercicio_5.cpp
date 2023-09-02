70bef58a9d9c5e707774aca07473813997b9bc6f
OutLaio <rlaio.ege@gmail.com>
2023-09-02 16:23:07 -0300

    Exercício a - Letra a)

---
+++ PI-P005/exercicio_5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+#include<bitset>
+
+using namespace std;
+
+int main(){
+
+    unsigned char gene;
+    
+    cout << "Digite o caractere correspondente ao gene: ";
+    cin >> gene;
+
+    return 0;
+}
