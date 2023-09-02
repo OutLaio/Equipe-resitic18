7dba14b761031b513adb446eb775202da624eafa
OutLaio <rlaio.ege@gmail.com>
2023-09-02 17:03:06 -0300

    Exercício 2 - Letra e)

--- PI-P006/exercicio_2.cpp
+++ PI-P006/exercicio_2.cpp
@@ -11,7 +11,7 @@ int main(){
     cout << "Digite outro valor inteiro: ";
     cin >> b;
 
-    c = 4*a + b/3 - 5;
+    c = 4*(a + b)/(3 - 5);
 
     cout << endl << "O resultado da expressao eh: " << c << endl << endl;
 
