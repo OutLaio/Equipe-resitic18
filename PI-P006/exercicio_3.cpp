56683cca95369489f3da6c721468ece901502e86
OutLaio <rlaio.ege@gmail.com>
2023-09-02 17:18:37 -0300

    Exercício 3 - Letra b)

--- PI-P006/exercicio_3.cpp
+++ PI-P006/exercicio_3.cpp
@@ -6,5 +6,10 @@ using namespace std;
 int main(){
     char ch1, ch2, ch3;
 
+    cout << endl << "Digite um caractere : ";
+    cin >> ch1;
+    cout << "Digite outro caractere : ";
+    cin >> ch2;
+
     return 0;
 }
