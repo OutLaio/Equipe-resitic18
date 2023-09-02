60901ced3abe6979285da4300a786866f72f293a
OutLaio <rlaio.ege@gmail.com>
2023-09-02 16:29:14 -0300

    Exercício 5 - Letra b)

--- PI-P005/exercicio_5.cpp
+++ PI-P005/exercicio_5.cpp
@@ -6,9 +6,19 @@ using namespace std;
 
 int main(){
     unsigned char gene;
+    int qtd = 0, i;
     
-    cout << "Digite o caractere correspondente ao gene: ";
+    cout << endl << "Digite o caractere correspondente ao gene: ";
     cin >> gene;
+
+    bitset<8> x(gene);
+    cout << "Os genes possuem a seguinte composicao: " << x << endl;
+
+    for (i = 0; i < 8; i++){
+        qtd += ((gene & 1<<i) == (1<<i)) ? 1 : 0;
+    }
+
+    cout << "Estao presentes " << qtd << " genes estudados!" << endl << endl;
 
     return 0;
 }
