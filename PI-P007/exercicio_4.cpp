5e92d57d61ef6f91e7041f8a2e5a4fcce10d6b0e
OutLaio <rlaio.ege@gmail.com>
2023-09-02 21:35:59 -0300

    Exercício 4

--- PI-P007/exercicio_4.cpp
+++ PI-P007/exercicio_4.cpp
@@ -3,8 +3,25 @@
 
 using namespace std;
 
-int main(){
+bool isPrimo(int num) {
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
+int main() {
+    int i;
+
+    cout << "Numeros primos de 1 a 100:" << endl;
+    for (i = 1; i <= 100; ++i) {
+        if (isPrimo(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
 
     return 0;
 }
