db25cae0937b1e3b2440e96872e7cb152a0a05d9
OutLaio <rlaio.ege@gmail.com>
2023-09-02 22:01:06 -0300

    Exercício 7

--- PI-P007/exercicio_7.cpp
+++ PI-P007/exercicio_7.cpp
@@ -1,10 +1,21 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main() {
+    int altura;
+    char ch = 'A';
 
+    cout << "Digite a altura do padrao: ";
+    cin >> altura;
+
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << ch;
+            ch++;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
