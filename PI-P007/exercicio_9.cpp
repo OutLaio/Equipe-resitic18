c2348463e5ad44995a36db253c3f36bf57279459
OutLaio <rlaio.ege@gmail.com>
2023-09-02 22:19:05 -0300

    Exercício 9

--- PI-P007/exercicio_9.cpp
+++ PI-P007/exercicio_9.cpp
@@ -1,10 +1,25 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main() {
+    int num, somaDivisores = 0;
 
+    cout << "Digite um numero inteiro positivo: ";
+    cin >> num;
+
+    if (num <= 0) {
+        cout << "O numero deve ser positivo." << endl;
+        return 1;
+    }
+
+    for (int i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            somaDivisores += i;
+        }
+    }
+
+    cout << endl << "O numero " << num << (somaDivisores==num ? " " : " nao ") << "eh um numero perfeito" << endl;
 
     return 0;
 }
