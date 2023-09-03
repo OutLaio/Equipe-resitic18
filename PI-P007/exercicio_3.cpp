fa291a61e3660e6da88b10deb2e408919f754ddf
OutLaio <rlaio.ege@gmail.com>
2023-09-02 21:29:41 -0300

    Exercício 3

--- PI-P007/exercicio_3.cpp
+++ PI-P007/exercicio_3.cpp
@@ -4,7 +4,20 @@
 using namespace std;
 
 int main(){
+    int numero, i;
 
+    cout << "Digite um numero inteiro: ";
+    cin >> numero;
+
+    cout << "Os divisores de " << numero << " sao: ";
+
+    for (i = 1; i <= numero; ++i) {
+        if (numero % i == 0) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
 
     return 0;
 }
