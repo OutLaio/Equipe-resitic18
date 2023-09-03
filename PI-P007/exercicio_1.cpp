dc1a5ed1de882e5083f9f6aa27bcb7f49f976018
OutLaio <rlaio.ege@gmail.com>
2023-09-02 21:14:52 -0300

    Exercício 1

---
+++ PI-P007/exercicio_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    int i;
+
+    for (i = 1; i <= 100; i++){
+        cout << i << " - ";
+        cout << (i%3==0 ? "Fizz" : "");
+        cout << (i%5==0 ? "Buzz" : "");
+        cout << endl;
+    }
+    return 0;
+}
