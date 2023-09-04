d8b60c3b45cc84251898bc2973ba9c8dca7eb4cb
OutLaio <rlaio.ege@gmail.com>
2023-09-04 15:06:04 -0300

    Exercício 2 - Letra a)

---
+++ PI-P008/exercicio_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    int vetor[100];
+
+    for (int i = 0; i < 100; i++){
+        vetor[i] = 1 + rand()%20;
+    }
+    
+
+    return 0;
+}
