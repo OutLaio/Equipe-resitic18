082c0667a6de98d08a5965d41909cef2d14faf58
OutLaio <rlaio.ege@gmail.com>
2023-09-04 17:16:17 -0300

    Exercício 2.1 - Letra b)

--- PI-P008/exercicio_2.1.cpp
+++ PI-P008/exercicio_2.1.cpp
@@ -5,11 +5,22 @@ using namespace std;
 
 int main(){
     int i;
-    float estacoes[250];
+    float estacoes[250], max = 0, min = 40;
 
     for (i = 0; i < 250; i++){
         estacoes[i] = 10.0 + (3.0*(rand()%100))/10.0;
     }
+
+    for (i = 0; i < 250; i++){
+        if (estacoes[i] > max){
+            max = estacoes[i];
+        }
+        if (estacoes[i] < min){
+            min = estacoes[i];
+        }
+    }
+    cout << "A maxima temperatura reportada foi " << max << "°C" << endl;
+    cout << "A minima temperatura reportada foi " << min << "°C" << endl;
 
     return 0;
 }
