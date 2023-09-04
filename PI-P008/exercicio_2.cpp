cee50348a5c398f7e1dd0d4045895540029db573
OutLaio <rlaio.ege@gmail.com>
2023-09-04 15:29:49 -0300

    Exercício 2 - Letra b)

--- PI-P008/exercicio_2.cpp
+++ PI-P008/exercicio_2.cpp
@@ -4,10 +4,24 @@
 using namespace std;
 
 int main(){
-    int vetor[100];
+    int i, j, vetor[100], repete[20];
 
-    for (int i = 0; i < 100; i++){
+    for (i = 0; i < 100; i++){
         vetor[i] = 1 + rand()%20;
+    }
+
+    for (i = 0; i < 20; i++){
+        repete[i] = 0;
+        for (j = 0; j < 100; j++){
+            if (vetor[j] == i+1){
+                repete[i] += 1; 
+            }
+        }
+        
+    }
+    
+    for (i = 0; i < 20; i++){
+        cout << "O numero " << i << " se repete " << repete[i] << " vezes." << endl;
     }
     
 
