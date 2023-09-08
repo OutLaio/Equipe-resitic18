194f95d1839deba4b344be9b619d6850d56a7ed7
OutLaio <rlaio.ege@gmail.com>
2023-09-08 16:33:11 -0300

    Exercício 3.1 - Letra b)

--- PI-P008/exercicio_3.1.cpp
+++ PI-P008/exercicio_3.1.cpp
@@ -13,6 +13,11 @@ int main(){
         str2.insert(i, a);
     }
 
+    a = toupper(str1.at(0));
+    str1.replace(str1.begin(), str1.begin()+1, a);
+    a = toupper(str2.at(0));
+    str2.replace(str2.begin(), str2.begin()+1, a);
+
     cout << endl << str1 << " - " << str2 << endl;    
 
     return 0;
