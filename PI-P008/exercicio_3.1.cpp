77bc202bd10c8a4c762e0e98979199f845437eb7
OutLaio <rlaio.ege@gmail.com>
2023-09-08 20:14:05 -0300

    Exercício 3.2

--- PI-P008/exercicio_3.1.cpp
+++ PI-P008/exercicio_3.1.cpp
@@ -1,28 +1,40 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 using namespace std;
 
 int main(){
-    string str1, str2, a;
+    int n = 10, k = 0;
+    bool flag;
+    vector<string> str;
+    string str1, a;
 
-    for (int i = 0; i < 10; i++){
-        a = 'a' + rand()%('z'-'a');
-        str1.insert(i, a);
-        a = 'a' + rand()%('z'-'a');
-        str2.insert(i, a);
+    for (int j = 0; j < n; j++){
+        for (int i = 0; i < 10; i++){
+            a = 'a' + rand()%('z'-'a');
+            str1.replace(str1.begin()+i, str1.end(), a);
+        }
+        a = toupper(str1.at(0));
+        str1.replace(0, 1, a);
+        str.insert(str.begin()+j, str1);
     }
+    
+    do{
+        flag = false;
+        k++;
+        for (int i = 0; i < n-k; i++){
+            if (str.at(i).compare(str.at(i+1))>0){
+                str.at(i).swap(str.at(i+1));
+                flag = true;
+            }
+        }
+    } while (flag);
+    
 
-    a = toupper(str1.at(0));
-    str1.replace(str1.begin(), str1.begin()+1, a);
-    a = toupper(str2.at(0));
-    str2.replace(str2.begin(), str2.begin()+1, a);
-
-    if(str1.compare(str2) < 0)
-        cout << endl << str1 << endl << str2 << endl;
-    else
-        cout << endl << str2 << endl << str1 << endl;
-
-
+    for (int i = 0; i < n; i++){
+        cout << str.at(i) << endl;
+    }
+    
     return 0;
 }
