eb6bbe99fcde9283563373ba7525066406317202
OutLaio <rlaio.ege@gmail.com>
2023-09-08 16:37:42 -0300

    Exercício 3.1 - Letra c)

--- PI-P008/exercicio_3.1.cpp
+++ PI-P008/exercicio_3.1.cpp
@@ -18,7 +18,11 @@ int main(){
     a = toupper(str2.at(0));
     str2.replace(str2.begin(), str2.begin()+1, a);
 
-    cout << endl << str1 << " - " << str2 << endl;    
+    if(str1.compare(str2) < 0)
+        cout << endl << str1 << endl << str2 << endl;
+    else
+        cout << endl << str2 << endl << str1 << endl;
+
 
     return 0;
 }
