e5369d85818702fcd1363cfe81ea36f310a859a2
Brendom Gonçalves <brendom.vi@gmail.com>
2023-09-08 16:35:12 -0300

    Etapa 2 completa

--- PI-P010/sistema_estudantes.cpp
+++ PI-P010/sistema_estudantes.cpp
@@ -12,6 +12,8 @@
 
 using namespace std;
 
+static void ordenaABC(vector<string> &palavras);
+
 int main(){
 
     int qntAlunos;
@@ -23,8 +25,9 @@ int main(){
     cout << "Digite quantos alunos serão cadastrados:" << endl << "> ";
     cin >> qntAlunos;
 
-    system(CLEAR_SCREEN); // limpa terminal
+    system(CLEAR_SCREEN); // Limpando terminal
 
+    // Preenchendo nomes e notas
     for(int i=0; i<qntAlunos; i++){
         string nomeAluno;
         float notaI, notaII;
@@ -41,5 +44,27 @@ int main(){
         notasAlunos.push_back(notaII);
     }
 
+    // Ordenando nomes
+    ordenaABC(nomeAlunos);
+
     return 0;
+}
+
+static void ordenaABC(vector<string> &palavras){
+    int n = palavras.size() - 1, cont = 0;
+    bool trocou;
+
+    do{
+        trocou = false;
+        for(int i=0; i<n; i++){
+            cont = 0;
+            while(palavras.at(i).at(cont) == palavras.at(i+1).at(cont)) cont++;
+            
+            if(tolower(palavras.at(i).at(cont)) > tolower(palavras.at(i+1).at(cont))){
+                swap(palavras.at(i), palavras.at(i+1));
+                trocou = true;
+            }
+        }
+        n--;
+    }while(trocou);
 }
