fad1dd6a997842b3bfba9bff04b9ab7d471c97c9
OutLaio <rlaio.ege@gmail.com>
2023-09-08 15:02:14 -0300

    Exercício 3 - Letra b)

--- PI-P008/exercicio_3.cpp
+++ PI-P008/exercicio_3.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main(){
     string data, temp;
     int dia, mes, ano;
+    bool erro = false;
 
     cout << "Digite uma data: ";
     cin >> data;
@@ -20,9 +21,45 @@ int main(){
     getline(str, temp, '/');
     ano = stoi(temp);
 
-    cout << "Dia: " << dia << endl
-         << "Mes: " << mes << endl
-         << "Ano: " << ano << endl;
+    if (dia < 1){
+        erro = true;
+    }
+
+    switch (mes){
+        case 2:
+            if (dia > 29)
+                erro = true;
+            break;
+        case 4:
+            if (dia > 30)
+                erro = true;
+            break;
+        case 6:
+            if (dia > 30)
+                erro = true;
+            break;
+        case 9:
+            if (dia > 30)
+                erro = true;
+            break;
+        case 11:
+            if (dia > 30)
+                erro = true;
+            break;
+        default:
+            if (dia > 31 || mes > 12)
+                erro = true;
+            break;
+    }
+    
+    if (erro){
+        cout << "Data invalida!!" << endl;
+    }else{
+        cout << "Dia: " << dia << endl
+            << "Mes: " << mes << endl
+            << "Ano: " << ano << endl;
+    }
+
 
     return 0;
 }
