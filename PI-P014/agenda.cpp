07585310bca90a31d0ba1b333a541bf3fe4634d0
OutLaio <rlaio.ege@gmail.com>
2023-09-10 19:34:19 -0300

    Lista 14, Exercícios 1, 2 e 3 completos, calculadora e agenda

---
+++ PI-P014/agenda.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<vector>
+#include<conio.h>
+#include<math.h>
+
+using namespace std;
+
+typedef struct T_contato{
+    string nome;
+    string telefone;
+} Contato;
+
+void adicionarContato(vector<Contato> *contatos) {
+    Contato novoContato;
+    string nome, telefone;
+
+    system("cls");
+    cout << "Digite o nome do contato: ";
+    cin.ignore();
+    getline(cin, nome);
+    cout << "Digite o telefone do contato: ";
+    getline(cin, telefone);
+
+    novoContato.nome = nome;
+    novoContato.telefone = telefone;
+
+    (*contatos).push_back(novoContato);
+
+    cout << "Contato adicionado com sucesso." << endl;
+    system("pause");
+    return;
+}
+
+
+
+
+void dispMenu(int posicao){
+    system("cls");
+    cout << "======= Agenda Simples =======" << endl << endl;
+    for (int i = 0; i < 4; i++){
+        if (i == posicao)
+            cout << "-> ";
+        switch (i){
+            case 0:
+                cout << "Adicionar Contato" << endl;
+                break;
+            case 1:
+                cout << "Remover Contato" << endl;
+                break;
+            case 2:
+                cout << "Listar Contatos" << endl;
+                break;
+            case 3:
+                cout << "Sair" << endl;
+                break;
+        default:
+            break;
+        }
+    }
+}
+
+void menuOpcoes(void){
+    int seta, posicao = 0;
+    vector<Contato> contatos;
+    do{
+        if(seta==80)
+            posicao++;
+        if(seta==72)
+            posicao--;
+        if(posicao>3)
+            posicao=0;
+        if(posicao<0)
+            posicao=3;
+
+        dispMenu(posicao);
+        seta = _getch();
+
+        if(seta == 13){
+            switch (posicao){
+            case 0:
+                adicionarContato(&contatos);
+                break;
+            case 1:
+                // removerContato(&contatos);
+                break;
+            case 2:
+                // listarContatos(&contatos);
+                break;
+            case 3:
+                seta = 27;
+                break;
+            default:
+                break;
+            }
+        }
+    } while (seta!=27);
+    
+}
+
+int main(){
+
+    menuOpcoes();
+    system("cls");
+    
+    return 0;
+}
---
+++ PI-P014/calculadora.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<vector>
+#include<math.h>
+
+using namespace std;
+
+double potenciacao(double base, double expoente) {
+    return pow(base, expoente);
+}
+
+double radiciacao(double numero, double indice) {
+    if (indice != 0) {
+        return pow(numero, 1.0 / indice);
+    } else {
+        cout << "Erro: Índice de raiz inválido!" << endl;
+        return NAN; 
+    }
+}
+
+int main() {
+    char operacao;
+    double num1, num2;
+
+    cout << "Calculadora Simples" << endl;
+    cout << "Operações disponíveis: +, -, *, /, ^ (potência), r (raiz)" << endl;
+
+    cout << "Digite a operação (+, -, *, /, ^, r): ";
+    cin >> operacao;
+
+    cout << "Digite o primeiro número: ";
+    cin >> num1;
+
+    if (operacao != 'r') {
+        cout << "Digite o segundo número: ";
+        cin >> num2;
+    }
+
+    double resultado;
+
+    switch (operacao) {
+        case '+':
+            resultado = adicao(num1, num2);
+            break;
+        case '-':
+            resultado = subtracao(num1, num2);
+            break;
+        case '*':
+            resultado = multiplicacao(num1, num2);
+            break;
+        case '/':
+            resultado = divisao(num1, num2);
+            break;
+        case '^':
+            resultado = potenciacao(num1, num2);
+            break;
+        case 'r':
+            cout << "Digite o índice da raiz: ";
+            cin >> num2;
+            resultado = radiciacao(num1, num2);
+            break;
+        default:
+            cout << "Operação inválida." << endl;
+            return 1;
+    }
+
+    if (!isnan(resultado)) {
+        cout << "Resultado: " << resultado << endl;
+    }
+
+    return 0;
+}
