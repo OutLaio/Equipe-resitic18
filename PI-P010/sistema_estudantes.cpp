bd57c0a1dec65c22250d2f386d4bec14247df6df
Brendom Gonçalves <brendom.vi@gmail.com>
2023-09-10 16:32:39 -0300

    Alteração de estrutura lógica do código

--- PI-P010/sistema_estudantes.cpp
+++ PI-P010/sistema_estudantes.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>
+#include <string.h>
+#include <algorithm>
 #include <iomanip>
+#include <cstdlib>
+
+using namespace std;
 
 #ifdef _WIN32
 #include <windows.h>
@@ -11,141 +15,188 @@
 #define CLEAR_SCREEN "clear"
 #endif
 
-using namespace std;
+// Variáveis globais
+/**
+ * 
+*/
+size_t qntEstudantes = 0, maiorNome = 0;
+bool alteracao = false;
+vector<string> nomesEstudantes;
+vector<float> notasEstudantes;
 
 /**
- * @brief Função estática para ordenar lexicográficamente lista de nomes.
- * @param palavras Vector<string> com nomes.
+ * @brief Função para controlar escolha do usuário em menus.
 */
-static void ordenaAbc(vector<string> &palavras);
-
+static void inicio();
+/**
+ * @brief Função para exibir menu incial.
+*/
+static void menuPrincipal();
+/**
+ * @brief Função para controlar inserção de estudantes.
+ * @param qntEstudantes Inteiro com a quantidade máxima de estudantes permitida.
+*/
+static void inserirEstudante();
+/**
+ * @brief Função para controlar exclusão de estudantes.
+ * @param qntEstudantes Inteiro com a quantidade máxima de estudantes permitida.
+*/
+static void excluirEstudante();
 /**
  * @brief Função estática para imprimir tabela com nomes e notas.
  * @param nomes Vector<string> com nomes.
  * @param notas Vector<float> com duas notas por aluno.
  * @param maiorNome Inteiro com valor do maior nome inserido no vector de nomes.
 */
-static void tabelaEstudantes(vector<string> nomes, vector<float> notas, int maiorNome);
-
+static void tabelaEstudantes();
 /**
- * @brief Função estática para controlar adição de alunos a lista de nomes.
- * @param resposta Booleano com controle sobre o loop do-while.
+ * @brief Função estática para ordenar lexicográficamente lista de nomes.
+ * @param palavras Vector<string> com nomes dos estudantes.
 */
-static void perguntaIncluir(bool &resposta);
+static void ordenaAbc(vector<string> &nomes);
 
-/**
- * @brief Função estática para controlar exclusão de estudantes.
- * @param resposta Booleano com controle sobre o loop do-while.
- * @param nomes Vector<string> com nomes.
- * @param notas Vector<float> com duas notas por aluno.
-*/
-static void perguntaRetirar(bool &resposta, vector<string> &nomesAlunos, vector<float> &notasAlunos);
-
+// INICO DO PROGRAMA
 int main(){
-
-    int maiorNome = 0, qntAlunos;
-    bool escolhaUsuario = true;
-    vector<string> nomeAlunos;
-    vector<float> notasAlunos;
-
-    cout << "Bem vindo ao Sistema de Notas dos Estudantes" << endl << endl;
-    cout << "Digite quantos alunos serão cadastrados:" << endl << "> ";
-    cin >> qntAlunos;
-
-    // Preenchendo nomes e notas
-    do{
-        system(CLEAR_SCREEN); // Limpando terminal
-
-        string nomeAluno;
-        float notaI, notaII;
-        cout << "Digite o nome do aluno:" << endl << "> ";
-        cin.ignore();
-        getline(cin, nomeAluno);
-
-        cout << endl << "Nota I: ";
-        cin >> notaI;
-        cout << "Nota II: ";
-        cin >> notaII;
-
-        // Obter maior nome inserido
-        if(nomeAluno.length() > maiorNome) maiorNome = nomeAluno.length();
-
-        nomeAlunos.push_back(nomeAluno);
-        notasAlunos.push_back(notaI);
-        notasAlunos.push_back(notaII);
-
-        if(nomeAlunos.size() >= qntAlunos){
-            cout << endl << "Quantidade máxima de alunos atingidas!" << endl << endl;
-            tabelaEstudantes(nomeAlunos, notasAlunos, maiorNome);
-            perguntaRetirar(escolhaUsuario, nomeAlunos, notasAlunos);
-        }
-        else {
-            perguntaIncluir(escolhaUsuario);
-            system(CLEAR_SCREEN); // Limpando terminal
-            if(!escolhaUsuario){
-                tabelaEstudantes(nomeAlunos, notasAlunos, maiorNome);
-                perguntaRetirar(escolhaUsuario, nomeAlunos, notasAlunos);
-            }
-        }
-
-    }while(escolhaUsuario && (nomeAlunos.size() < qntAlunos));
-
-    // Ordenando nomes
-    ordenaAbc(nomeAlunos);
-
-    tabelaEstudantes(nomeAlunos, notasAlunos, maiorNome);
-
+    inicio();
     return 0;
 }
-static void perguntaRetirar(bool &resposta, vector<string> &nomesAlunos, vector<float> &notasAlunos){
-    char resp;
-    do{
-        cout << endl << "Desejan excluir algum aluno (s/n)" << endl << "> ";
-        cin.ignore();
-        cin >> resp;
-    }while(resp != 's' && resp != 'n');
+
+static void inicio(){
+
+    int escolhaUsuario;
+    system(CLEAR_SCREEN);
     
-    if(resp == 's'){
+    do{
+        cout << "\t **CADASTRO DE ESTUDANTES **" << endl << endl;
+        cout << "Defina um limite de estudantes [1 a X]: ";
+        cin >> qntEstudantes;
+        if(qntEstudantes < 1) cout << "ERROR: Limite precisa ser poitivo e maior que 0!" << endl << endl;
+    }while(qntEstudantes < 1);
+    
+    system(CLEAR_SCREEN);
+
+    do{
+        escolhaUsuario = 0;
+
+        menuPrincipal();
+        cin >> escolhaUsuario;    
         
-    } else{
-        resposta = false;
-    }
+        switch(escolhaUsuario){
+            case 1:
+                if(nomesEstudantes.size() < qntEstudantes)
+                    inserirEstudante();
+                else
+                    cout << "ERROR: Limite de estudantes atingidos!" << endl;
+                break;
+            case 2:
+                excluirEstudante();                
+                break;
+            case 3:
+                if(nomesEstudantes.size() > 0) tabelaEstudantes();
+                else cout << "Tabela vazia! Insira um estudante." << endl;
+                break;
+            case 4:
+                cout << "Encerrando sistema..." << endl;
+                break;
+            default:
+                cout << endl << "ERROR: Nenhuma opção escolhida. Digite novamente!" << endl << endl;
+                break;
+        }
+    }while(escolhaUsuario != 4);
+
 }
-static void perguntaIncluir(bool &resposta){
-    char resp;
-    do{
-        cout << endl << "Desejan incluir mais alunos (s/n)" << endl << "> ";
-        cin.ignore();
-        cin >> resp;
-    }while(resp != 's' && resp != 'n');
-    if(resp == 'n') resposta = false;
+static void menuPrincipal(){
+    cout << endl << "Armazenado/Limite: [" << nomesEstudantes.size() << "/" << qntEstudantes << "]" << endl;
+    cout << endl << "Escolha [1 a 4]" << endl;
+    cout << "1. Adicionar estudante" << endl;
+    cout << "2. Excluir estudante" << endl;
+    cout << "3. Tabela estudantes" << endl;
+    cout << "4. Sair" << endl;
+    cout << "> ";
 }
-static void tabelaEstudantes(vector<string> nomes, vector<float> notas, int maiorNome){
-    int contador = 0;
+static void inserirEstudante(){
+    string nome;
+    float notaI, notaII;
+
+    system(CLEAR_SCREEN);
+
+    cout << "Digite o nome do aluno:" << endl << "> ";
+    cin.ignore();
+    getline(cin, nome);
+
+    cout << endl << "Nota I: ";
+    cin >> notaI;
+
+    cout << "Nota II: ";
+    cin >> notaII;
+
+    // Obter maior nome inserido
+    if(nome.length() > maiorNome) maiorNome = nome.length();
     
-    system(CLEAR_SCREEN); // Limpando terminal
+    nomesEstudantes.push_back(nome);
+    notasEstudantes.push_back(notaI);
+    notasEstudantes.push_back(notaII);
+    
+    alteracao = true; // Controle de alteração da lista;
+
+    cout << "Estudante cadastrado com sucesso!" << endl;
+}
+static void excluirEstudante(){
+    string nome = "";
+    bool estudanteEncontrado = false;
+    vector<string>::iterator itNomes;
+    vector<float>::iterator itNotas;
+    
+    system(CLEAR_SCREEN);
+
+    tabelaEstudantes();
+
+    cout << endl << "Obs: Atente-se a escrita do nome." << endl;
+    cout << "Digite o nome do aluno que irá deletar:" << endl << "> ";
+    cin.ignore();
+    cin >> nome;
+
+    for(size_t i=0; i<nomesEstudantes.size(); i++){
+        if(nomesEstudantes.at(i) == nome){
+            estudanteEncontrado = true;
+            nomesEstudantes.erase(nomesEstudantes.begin()+i);
+            notasEstudantes.erase(notasEstudantes.begin()+(i*2));
+            notasEstudantes.erase(notasEstudantes.begin()+((i*2)+1));
+        }
+    }
+
+    if(estudanteEncontrado) cout << "Aluno deletado com sucesso!" << endl;
+    else cout << "Aluno não encontrado. Verifique se digitou corretamente." << endl;
+}
+static void tabelaEstudantes(){
+    int contador = 0;
+
+    system(CLEAR_SCREEN);
+
+    if(alteracao) ordenaAbc(nomesEstudantes);
 
     cout << endl << "** DADOS DOS ALUNOS **";
     cout << endl << "Nomes" << setw(maiorNome + 5) << "Nota I" << setw(10) << "Nota II" << endl;
-    for(string nome : nomes){
+    for(string nome : nomesEstudantes){
         cout << nome << setw(maiorNome - nome.length() + 10)
-        << notas.at(contador) << setw(10)
-        << notas.at(contador + 1) << endl;
+        << notasEstudantes.at(contador) << setw(10)
+        << notasEstudantes.at(contador + 1) << endl;
         contador += 2;
     }
 }
-static void ordenaAbc(vector<string> &palavras){
-    int n = palavras.size() - 1, cont = 0;
+static void ordenaAbc(vector<string> &nomes){
+    int n = nomes.size() - 1;
+    int contador = 0;
     bool trocou;
 
     do{
         trocou = false;
         for(int i=0; i<n; i++){
-            cont = 0;
-            while(palavras.at(i).at(cont) == palavras.at(i+1).at(cont)) cont++;
+            contador = 0;
+            while(nomes.at(i).at(contador) == nomes.at(i+1).at(contador)) contador++;
             
-            if(tolower(palavras.at(i).at(cont)) > tolower(palavras.at(i+1).at(cont))){
-                swap(palavras.at(i), palavras.at(i+1));
+            if(tolower(nomes.at(i).at(contador)) > tolower(nomes.at(i+1).at(contador))){
+                swap(nomes.at(i), nomes.at(i+1));
                 trocou = true;
             }
         }
